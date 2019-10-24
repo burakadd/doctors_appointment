0da1720018398ddb6f0f7f270c0a850b23a0426c
burakadd <6ypu4.92@gmail.com>
2019-10-24 22:23:09 +0300

    test bot

--- Bot.py
+++ Bot.py
@@ -3,8 +3,15 @@ import telebot
 mybot = telebot.TeleBot('880698305:AAGp4i7-mbskWv2TGITTLF7tHnm77wawFho')
 
 
-@bot.message_handler(commands=['start'])
+@mybot.message_handler(commands=['start'])
 def start_message(message):
     mybot.send_message(message.chat.id, 'Hello')
 
+
+@mybot.message_handler(commands=['finish'])
+def start_message(message):
+    mybot.send_message(message.chat.id, 'Bye')
+
+
 mybot.polling()
+
